155667a02f3d089b65373d504b7dddaa4ac2979f
Abdukarimovic <144595148+Abdukarimovic@users.noreply.github.com>
2024-02-07 18:54:39 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routing from "./routes";
+
+function App(props) {
+
+    return (
+        <div>
+            <Routing/>
+
+        </div>
+    );
+}
+
+export default App;
